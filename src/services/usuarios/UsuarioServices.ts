1ce6c06cfd73be85f6876056b7a84cde9c53e629
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-24 17:26:16 -0300

    :sparkles: feat: update API base URL and enhance UsuarioServices with new endpoint for fetching all users

--- src/services/usuarios/UsuarioServices.ts
+++ src/services/usuarios/UsuarioServices.ts
@@ -5,8 +5,12 @@ class UsuarioServices {
     user: Usuario[] = [];
 
     async getUsuarios() {
-        const reponse = await api.get<Usuario[]>('/usuarios')
+        const reponse = await api.get<Usuario[]>('/usuarios/all')
         this.user = reponse.data
+    }
+
+    async postUsuario(usuario: Usuario) {
+        await api.post('/usuarios', usuario)
     }
 }
 
