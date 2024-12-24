8a4009da34f1a684cff1a809f0d53df14b24dcd2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-24 18:26:15 -0300

    :sparkles: feat: add styling for Login component and update structure in Login.tsx

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,13 @@
+import './Login.css'
+
 function Login(){
     return (
-        <section>
+        <section className="login">
             <h1>Login</h1>
             <form>
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <button type='button'>Entrar</button>
+                <input type="text" placeholder="Email" className="form" />
+                <input type="password" placeholder="Senha" className="form" />
+                <button type='button' className="form" >Entrar</button>
             </form>
         </section>
     )
