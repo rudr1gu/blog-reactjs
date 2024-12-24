03793ead0bc531c83ed97469fcc0714f5eeb1c0e
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-24 13:50:14 -0300

    :sparkles: feat: add user registration feature with user list display

--- src/components/cadastro/Cadastro.tsx
+++ src/components/cadastro/Cadastro.tsx
@@ -1,16 +1,34 @@
 import './Cadastro.css'
+import Usuario from './User'
 
 function Cadastro(){
+    const user1: Usuario = new Usuario(1,'Rodrigo', 'rodrigo@email.com', 'Senha123');
+    const user2: Usuario = new Usuario(2,'João', 'jao@email.com', 'Senha123');
+    const users = [user1, user2]
+
     return (
         <section className="cadastro">
             <h1 className='titulo-cadastro'>Cadastro de Usuario</h1>
             <form>
-                <input type="text" placeholder="Nome" className='form'/>
-                <input type="text" placeholder="Email" className='form'/>
-                <input type="password" placeholder="Senha" className='form'/>
-                <button className='form'>Cadastrar</button>
+                <input name='nome' type="text" placeholder="Nome" className='form'/>
+                <input name='user' type="text" placeholder="Email" className='form'/>
+                <input name='senha' type="password" placeholder="Senha" className='form'/>
+                <button type='button' className='form'>Cadastrar</button>
             </form>
+
+        { users.map((user) => (
+            <article key={user.id}>
+            <h2>Usuarios Cadastrados</h2>
+            <ul>
+                <li>Nome: {user.nome}</li>
+                <li>E-mail: {user.email} </li>
+            </ul>
+            <button className='lixeira'>lixeira</button>
+            </article>
+            )            
+        )}
         </section>
+        
     )
 }
 
--- src/components/home/App.tsx
+++ src/components/home/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 
+
 function App() {
+  
   return (
-    <h1>Olá Mundo</h1>
+    <h1>nada por aqui</h1>
   )
 }
 
