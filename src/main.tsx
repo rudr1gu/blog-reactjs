0f0da451b14d04c83051003bd21e8517ba2268e7
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-25 12:06:09 -0300

    :sparkles: feat: add react-router-dom for routing and update App component structure

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,27 @@ import App from './components/home/App.tsx'
 import Cadastro from './components/cadastro/Cadastro.tsx'
 import Login from './components/login/Login.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />
+  },
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Cadastro />
-    <Login />
+    <RouterProvider router={router}> 
+    </RouterProvider>
   </StrictMode>,
 )
